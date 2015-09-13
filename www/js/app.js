240d78e83744b82b99cdc9bccb80c2b614c4ee3a
zitro06 <zitro06@gmail.com>
2015-09-12 22:25:13 -0500

    lista de modeloramas df

--- www/js/app.js
+++ www/js/app.js
@@ -8,7 +8,7 @@ var _HOST_PUSH_SERVER = "http://192.168.1.72:1337";
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.Modelorama', 'ngCordova'])
 
 .run(function($ionicPlatform, $cordovaLocalNotification, $rootScope) {
   $ionicPlatform.ready(function() {
@@ -98,7 +98,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
     url: '/search',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'templates/search.html',
+         controller: 'ModeloramasCtrl'
       }
     }
   })
